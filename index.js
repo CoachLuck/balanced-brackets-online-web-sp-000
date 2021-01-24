22a852005518e522e06fab7db22bf83730730426
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-24 03:54:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,14 +4,32 @@ function isBalanced(str) {
   return isBal(str, str[0], 0)
 }
 
+// function isBal(str, openKey, idx) {
+//   if (!close.has(str[0])) return false
+// 
+//   let cur = str[idx]
+//   let next = str[idx + 1]
+//   let prev = str[idx - 1]
+//   if (isOpen(cur)) {
+//     if (next == close.get(cur)) {
+//       str = str.substring(0, idx) + str.slice(idx + 2)
+//       return str.length > 1 ? isBal(str, str[0], 0) : true
+//     }
+// 
+//     return isBal(str, cur, idx + 1)
+//   }
+// 
+//   return close.get(prev) != cur ? isBal(str, prev, idx + 1) : false
+// }
+
 function isBal(str, openKey, idx) {
-  if (!close.has(str[0])) return false
+  if (isClose(str[0])) return false
 
   let cur = str[idx]
   let next = str[idx + 1]
   let prev = str[idx - 1]
   if (isOpen(cur)) {
-    if (next == close.get(cur)) {
+    if (next == getClose(cur)) {
       str = str.substring(0, idx) + str.slice(idx + 2)
       return str.length > 1 ? isBal(str, str[0], 0) : true
     }
@@ -19,26 +37,8 @@ function isBal(str, openKey, idx) {
     return isBal(str, cur, idx + 1)
   }
 
-  return close.get(prev) != cur ? isBal(str, prev, idx + 1) : false
+  return getClose(prev) != cur ? isBal(str, prev, idx + 1) : false
 }
-
-// function isBal(str, openKey, idx) {
-//   if (isClose(str[0])) return false
-//
-//   let cur = str[idx]
-//   let next = str[idx + 1]
-//   let prev = str[idx - 1]
-//   if (isOpen(cur)) {
-//     if (next == getClose(cur)) {
-//       str = str.substring(0, idx) + str.slice(idx + 2)
-//       return str.length > 1 ? isBal(str, str[0], 0) : true
-//     }
-//
-//     return isBal(str, cur, idx + 1)
-//   }
-//
-//   return getClose(prev) != cur ? isBal(str, prev, idx + 1) : false
-// }
 
 function getClose(c) {
   switch (c) {
