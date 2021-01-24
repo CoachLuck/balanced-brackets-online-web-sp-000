75945fc5eca78a6049573223fa011c10da981c2f
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-24 03:49:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -71,6 +71,22 @@ console.log(9, isBalanced(insaneLong + "}" + insaneLong), "to be false")
 var newTime = new Date().getTime() - time
 console.log(`Done (${newTime} ms)`)
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function demo() {
+  console.log('Taking a break...');
+  await sleep(2000);
+  console.log('Two seconds later, showing sleep in a loop...');
+
+  // Sleep in loop
+  for (let i = 0; i < 5; i++) {
+    if (i === 3)
+      await sleep(2000);
+    console.log(i);
+  }
+}
 
 var loopTime = new Date().getTime()
 var crazyLong = insaneLong + insaneLong + insaneLong + insaneLong
